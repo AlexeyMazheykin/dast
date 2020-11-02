5578574265897b3a0bc8b95f5445a5ef6e9552df
Alexey Mazheykin <mazheykin@icloud.com>
2020-11-02 10:39:26 +0300

    slaider beta

--- js/main.js
+++ js/main.js
@@ -19,32 +19,76 @@ window.addEventListener('scroll', () => {
 const carousel = document.querySelector('.carousel');
 const slider = document.querySelector('#slider');
 const choice = document.querySelector('.hero__choice');
-let direction;
+let direction = -1;
 let count = 0;
-const choseItems = document.querySelectorAll('.hero__choice-item');
+/*const choseItems = document.querySelectorAll('.hero__choice-item');*/
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
 
-choseItems.forEach(function (el) {
-el.addEventListener('click', ()=> {
-    
-})
-})
-choice.addEventListener('click', function() {
+/*choseItems.forEach(function (el) {
+    el.addEventListener('click', () => {
+        console.log(count, el.dataset.id)
+        if (count > el.dataset.id) {
+            console.log('Счетчик больше')
+
+            if (direction === -1) {
+                direction = 1;
+                slider.appendChild(slider.firstElementChild);
+            }
+            carousel.style.justifyContent = 'flex-end';
+            slider.style.transform = `translate(${(count - el.dataset.id)*20}%)`;
+            count = Number(el.dataset.id);
+            console.log(count, el.dataset.id)
+
+        } else if (count < el.dataset.id) {
+            console.log('счетчик меньше')
+            if (count > 4) {
+                count = 0
+            }
+            direction = -1;
+            carousel.style.justifyContent = 'flex-start';
+            slider.style.transform = `translateX(-${(el.dataset.id - count)*20}%)`;
+            count = Number(el.dataset.id);
+            console.log(count, el.dataset.id)
+        }
+    })
+})*/
+
+next.addEventListener('click', function () {
+    if (direction === 1) {
+        //  direction = -1;
+        slider.prepend(slider.lastElementChild);
+    }
     direction = -1;
     carousel.style.justifyContent = 'flex-start';
     slider.style.transform = 'translateX(-20%)';
 });
 
+prev.addEventListener('click', function () {
+    if (direction === -1) {
+        direction = 1;
+        slider.appendChild(slider.firstElementChild);
+    }
+    carousel.style.justifyContent = 'flex-end';
+    slider.style.transform = 'translateX(20%)';
 
-setInterval(function () {
+});
+
+/*setInterval(function () {
     count++
-    if (count === 4) {
+    if (count > 4) {
         count = 0
+    }
+    if (direction === 1) {
+        //  direction = -1;
+        slider.prepend(slider.lastElementChild);
     }
     direction = -1;
     carousel.style.justifyContent = 'flex-start';
     slider.style.transform = 'translateX(-20%)';
-}, 10000)
-slider.addEventListener('transitionend', function() {
+}, 100000)*/
+
+slider.addEventListener('transitionend', function () {
     // get the last element and append it to the front
 
     if (direction === 1) {
@@ -54,10 +98,10 @@ slider.addEventListener('transitionend', function() {
     }
 
     slider.style.transition = 'none';
-    slider.style.transform = 'translate(0)';
+    slider.style.transform = `translateX(0)`;
     setTimeout(() => {
         slider.style.transition = 'all 0.5s';
     })
-},false);
+}, false);
 
 
