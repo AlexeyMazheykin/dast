cff32b96b5a57cdd1591d88cab05a7b1b5adfe66
Alexey Mazheykin <mazheykin@icloud.com>
2020-11-05 17:23:36 +0300

    jhfjhg

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const headerBottom = document.querySelector('.header__bottom');
 const headerTop = document.querySelector('.header__top');
 const main = document.getElementById('main');
+const hero = document.querySelector('.hero');
 
 
 window.addEventListener('scroll', () => {
@@ -16,65 +17,47 @@ window.addEventListener('scroll', () => {
 
 //SLIDER
 
-const carousel = document.querySelector('.carousel');
+const sliderText = document.querySelector('#slider').innerHTML;
 const slider = document.querySelector('#slider');
 const choice = document.querySelector('.hero__choice');
-const choseItems = document.querySelectorAll('.hero__choice-item');
-let direction = 1;
-let count = 0;
-let countChild = 0;
+const choiceItems = document.querySelectorAll('.hero__choice-item');
+let countHero = 0;
+let countInt = 4;
+let countListener = 1;
 
-
-choseItems.forEach(element => {
-    element.addEventListener('click', function () {
-        clearInterval(interval);
-
-        if (count < this.dataset.id) {
-            direction = 1;
-            carousel.style.justifyContent = 'flex-start';
-            slider.style.transform = `translateX(${-(+this.dataset.id - count) * 1900}px)`;
-            countChild = +this.dataset.id - count;
-            count = +this.dataset.id;
-        } else if (count > this.dataset.id) {
-            direction = -1;
-            carousel.style.justifyContent = 'flex-end';
-            countChild = count - Number(this.dataset.id);
-            count = +this.dataset.id;
-            for (let i = 0; i < countChild; i++) {
-                slider.appendChild(slider.firstElementChild);
+choiceItems.forEach(el => {
+    el.addEventListener('click', function () {
+        if (countHero <= 4) {
+            countHero = +this.dataset.id
+            slider.style.left = `-${100 * (+this.dataset.id)}%`
+        } else {
+            slider.style.left = `-${100 * (+this.dataset.id)}%`
+            if (countInt / countListener < +this.dataset.id * countListener) {
+                countInt = +this.dataset.id * countListener + countInt;
+            } else if (countInt / countListener > +this.dataset.id * countListener) {
+                countInt = countInt - +this.dataset.id;
             }
-            slider.style.transform = `translateX(${(count - Number(this.dataset.id)) * 1900}px)`;
         }
     });
 });
-
-let interval = setInterval(function () {
-    count++
-    if (count > 4) {
-        count = 0;
-    }
-    countChild = 1;
-    slider.style.transform = `translateX(-1900px)`;
-}, 10000);
-
-slider.addEventListener('transitionend', function () {
-    console.log('Событие', count, countChild)
-
-    if (direction === -1) {
-        for (let i = 0; i < countChild; i++) {
-            slider.prepend(slider.lastElementChild);
-        }
+setInterval(function () {
+    if (countHero < 4) {
+        countHero++
+        countInt = 4;
+        slider.style.left = `-${100 * (countHero)}%`
     } else {
-        for (let i = 0; i < countChild; i++) {
-            slider.appendChild(slider.firstElementChild);
+        countHero = 5;
+        countInt++;
+        slider.style.left = `-${100 * (countInt)}%`
+        console.log(countHero, countInt, countListener)
+        if (countInt % 5 === 0) {
+            countListener++
+            slider.insertAdjacentHTML('beforeend', sliderText);
         }
     }
-    slider.style.transition = 'none';
-    slider.style.transform = `translateX(0px)`;
-    setTimeout(() => {
-        slider.style.transition = 'all 0.5s';
-    })
-}, false);
+},20000);
+
+
 
 //BEST
 
@@ -89,7 +72,6 @@ bestBtnPrev.style.display = 'none';
 bestBtn.forEach(function (element) {
     element.addEventListener('click', function () {
         bestCount += +this.dataset.count;
-        console.log(bestCount);
         sliderPosition += +this.dataset.trans
         bestSlider.style.left = `${sliderPosition}%`;
         if (sliderPosition == 0) {
@@ -104,6 +86,37 @@ bestBtn.forEach(function (element) {
         }
     });
 });
+//SCROLL TO TOP
+
+const scrollTop = document.querySelector('.scroll__top');
+let scrolled;
+let timer;
+
+window.addEventListener('scroll', () =>{
+    if (window.pageYOffset > hero.scrollHeight) {
+        scrollTop.style.display = 'block';
+    } else {
+        scrollTop.style.display = 'none';
+    }
+});
+scrollTop.addEventListener('click', () => {
+
+scrolled = window.pageYOffset;
+    scrollToTop();
+});
+function scrollToTop () {
+    if (scrolled > 0) {
+        window.scrollTo(0, scrolled);
+        scrolled = scrolled - 50;
+        timer = setTimeout(scrollToTop, 10);
+    } else {
+        window.scrollTo(0, 0);
+        clearTimeout(timer)
+    }
+
+}
+
+
 
 
 
